05c4994a21ac28dbec87645fd6fb31155a8ea53c
dnoak <daniel.ortega.carvalho@gmail.com>
2022-09-18 20:52:52 -0400

    commit 2

--- convolucao.py
+++ convolucao.py
@@ -1,23 +1,3 @@
-# Aluno: Daniel Ortega de Carvalho, RA:170088
-# Aluno: Gabriel Hiroaki da Silva Kanezaki, RA:179292
-
-######### INSTRUÇÕES ##########
-# Execute o programa
-
-# 1 - Selecione o tipo de convolução, depois aperte
-# 2 - em OK e arraste e veja a convolução sendo efetuada
-
-# * * * * * * * * * * * * * * * * * * * * * * * * * * 
-# Caso deseje efetuar outro tipo de convolução
-# utilize a matriz de Kernel customizada e crie
-# sua própria convolução, depois aperte em OK
-# e arraste e veja a convolução sendo efetuada
-# * * * * * * * * * * * * * * * * * * * * * * * * * *
-# Toda a vez que você selecionar o tipo de convolução
-# ou criar um Kernel existente, ele resetará sua
-# convolução na etapa 0, possibilitando você refazer
-# várias vezes a convolução
-
 from tkinter import font
 import numpy as np
 import tkinter as tk
